5302c6bae96d65bf5a78c1161f9889f78afb7ae6
ShotOfVodkah <PCA12345@Yandex.ru>
2025-06-13 09:45:03 +0300

    Initial Commit

---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by Stepan Polyakov on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
