f5b6cb84c88a2029e9d69deff8f1a5c4f9c42f4e
ShotOfVodkah <PCA12345@Yandex.ru>
2025-06-26 13:51:41 +0300

    enum fix

--- Finance/Screens/AccountView/AccountViewModel.swift
+++ Finance/Screens/AccountView/AccountViewModel.swift
@@ -7,28 +7,6 @@
 
 import Foundation
 
-enum Currency: String, CaseIterable {
-    case rub = "RUB"
-    case usd = "USD"
-    case eur = "EUR"
-    
-    var symbol: String {
-        switch self {
-        case .rub: return "₽"
-        case .usd: return "$"
-        case .eur: return "€"
-        }
-    }
-    
-    var fullName: String {
-        switch self {
-        case .rub: return "Российский рубль ₽"
-        case .usd: return "Американский доллар $"
-        case .eur: return "Евро €"
-        }
-    }
-}
-
 
 @MainActor
 final class AccountViewModel: ObservableObject {
