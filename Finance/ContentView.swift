560731b4b7403ac3d3bd70e8793b051b9ce00573
ShotOfVodkah <PCA12345@Yandex.ru>
2025-07-09 02:30:54 +0300

    create

--- Finance/ContentView.swift
+++ Finance/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
     @State var selectedTab = 0
     let transactionsService = TransactionsService()
     let categoriesService = CategoriesService()
